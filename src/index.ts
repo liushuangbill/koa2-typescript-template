477a4eec90a3dabc70341550b87975f9579ca969
liushuang <liushuangbill@outlook.com>
2019-06-04 16:51:56 +0800

    add bodyparser

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import * as Koa from 'koa'
+import * as bodyparser from 'koa-bodyparser'
 import router from './controller'
 
 const app = new Koa()
 
+app.use(bodyparser())
 app
   .use(router.routes())
   .use(router.allowedMethods())
